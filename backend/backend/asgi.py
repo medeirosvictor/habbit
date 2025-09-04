dbf3ae4bb8c17e880548de96acf16f0b10e94942
Victor Medeiros <victoramedeiros@gmail.com>
2025-09-04 13:43:57 -0400

    be configs

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+settings_module = 'backend.deployment_settigns' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
